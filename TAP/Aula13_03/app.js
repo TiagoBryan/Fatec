03895756df890b16cbe5ce8095281a58e3472118
Your Name <you@example.com>
2025-03-20 21:08:32 -0300

    feat: metodo post adicionado

--- TAP/Aula13_03/app.js
+++ TAP/Aula13_03/app.js
@@ -8,10 +8,25 @@ app.engine("handlebars", handlebars({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 app.get("/", function(req, res){
     res.render("primeira_pagina")
+})
+
+app.post("/cadastrar", function(req, res){
+    post.create({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao,
+    }).then(function(){
+        res.redirect('/')
+
+    }).catch(function(erro){
+        res.send('Erro ao criar post: ' + erro)
+    })
 })
 
 app.listen(8081, function(){
