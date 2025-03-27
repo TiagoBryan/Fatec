d50d13c60ab9bda6509cbc0a27c76d677b69f5e2
TiagoBryan <tiagobryanroliveira@gmail.com>
2025-03-27 15:13:20 -0300

    feat: Adicionada funcionalidade de excluir e editar

--- TAP/Aula13_03/app.js
+++ TAP/Aula13_03/app.js
@@ -3,6 +3,7 @@ const app = express()
 const handlebars = require("express-handlebars").engine
 const bodyParser = require('body-parser')
 const post = require('./models/post')
+const handlebarsHelper = require("handlebars");
 
 app.engine("handlebars", handlebars({defaultLayout: "main"}))
 app.set("view engine", "handlebars")
@@ -21,6 +22,55 @@ app.get("/consulta", async function (req, res) {
     } catch (error) {
         res.send("Erro ao buscar agendamentos: " + error);
     }
+});
+
+
+app.get("/editar/:id", async function (req, res) {
+    try {
+        const agendamento = await post.findByPk(req.params.id);
+        if (!agendamento) {
+            return res.send("Agendamento não encontrado.");
+        }
+        res.render("editar_pagina", { agendamento: agendamento.toJSON() });
+    } catch (error) {
+        res.send("Erro ao buscar agendamento: " + error);
+    }
+});
+
+app.post("/editar/:id", async function (req, res) {
+    try {
+        await post.update(
+            {
+                nome: req.body.nome,
+                telefone: req.body.telefone,
+                origem: req.body.origem,
+                data_contato: req.body.data_contato,
+                observacao: req.body.observacao,
+            },
+            {
+                where: { id: req.params.id }
+            }
+        );
+        res.redirect("/consulta");
+    } catch (error) {
+        res.send("Erro ao atualizar agendamento: " + error);
+    }
+});
+
+app.get("/excluir/:id", async function (req, res) {
+    try {
+        await post.destroy({
+            where: { id: req.params.id }
+        });
+        res.redirect("/consulta");
+    } catch (error) {
+        res.send("Erro ao excluir agendamento: " + error);
+    }
+});
+
+
+handlebarsHelper.registerHelper("eq", function (a, b) {
+    return a === b;
 });
 
 
