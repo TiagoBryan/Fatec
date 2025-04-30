7af389b3834d4d6ae5cded765e1d6221fbee5c73
TiagoBryan <tiagobryanroliveira@gmail.com>
2025-04-30 15:33:14 -0300

    Funcao HASH adaptada

---
+++ Estrutura_de_Dados/Hash/Hash.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mainhash;
+
+/**
+ *
+ * @author TiaguinhoDaGoiaba
+ */
+public class Hash {
+
+    public double item;
+    public boolean ocupado;
+    public boolean removido;
+
+    public Hash(boolean b) { // construtor
+        ocupado = false;
+        removido = false;
+    }
+}
